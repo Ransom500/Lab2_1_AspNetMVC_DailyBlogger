32c1c3891d3cad8e7896337cda348b595c311ff1
Ransom500 <Ransomtojoshua@gmail.com>
2020-08-26 16:23:43 -0400

    Add project files.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab2_1_AspNetMVC_DailyBlogger.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab2_1_AspNetMVC_DailyBlogger.Controllers
+{
+    public class BlogController : Controller
+    {
+        private BlogContext _context;
+
+        public BlogController(BlogContext context)
+        {
+            _context = context;
+        }
+        public IActionResult List()
+        {
+            IEnumerable<BlogPost> posts = _context.blogPost.ToList<BlogPost>();
+            return View(posts);
+        }
+
+       
+    }
+}
